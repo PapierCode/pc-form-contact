2b06818441197bdd8c17682b190967e96130e804
papiercode <julien@papier-code.fr>
2021-11-29 11:36:57 +0100

    3.6.0 ajout action before & after formulaire

--- form/form-template.php
+++ form/form-template.php
@@ -11,6 +11,10 @@ global $pc_contact_form;
 
 if ( is_object( $pc_contact_form ) ) {
 
+	do_action( 'pc_action_before_form_contact', $pc_contact_form );
+
 	$pc_contact_form->display_form();
+
+	do_action( 'pc_action_after_form_contact', $pc_contact_form );
 	
 }
--- pc-form-contact.php
+++ pc-form-contact.php
@@ -3,7 +3,7 @@
 Plugin Name: [PC] Form Contact
 Plugin URI: www.papier-code.fr
 Description: Formulaire de contact
-Version: 3.5.0
+Version: 3.6.0
 Author: Papier Codé
 */
 
